8da8222211f3df6a19228b8aa3661d79ac2e62ac
reecejr <reecejrobertson@gmail.com>
2022-04-06 19:44:40 -0600

    Testing alternate values in supercomputer file.

--- supercomputer.py
+++ supercomputer.py
@@ -10,13 +10,13 @@ from matplotlib import pyplot as plt
 M = 10
 
 # Define the number of qubits to simulate for each experiment.
-N_ADDITION = 35
-N_QFT = 35
-N_QPE = 35
+N_ADDITION = 10#35
+N_QFT = 20#35
+N_QPE = 20#35
 N_ARITHMETIC = 3
-N_MULTIPLICATION = 17
-N_EM_QFT = 40
-N_EM_QPE = 40
+N_MULTIPLICATION = 8#17
+N_EM_QFT = 20#40
+N_EM_QPE = 20#40
 
 # ---------------------------------------------------------------------------- #
 #                                   Addition                                   #
