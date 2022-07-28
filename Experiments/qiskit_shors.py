c38982bda053e336b7fce58bb26818ae1366096f
reecejr <reecejrobertson@gmail.com>
2022-07-28 09:36:36 -0600

    Added IBM Shor's algorithm experiment.

---
+++ Experiments/qiskit_shors.py
@@ -0,0 +1,22 @@
+from qiskit import IBMQ
+from qiskit.utils import QuantumInstance
+from qiskit.algorithms import Shor
+import time
+
+IBMQ.enable_account('99c0e0cf0828e07052aa037e95f66583b58e649173193b0743e9eeb3458cb4ec5ce2980423e36d2a13ca1a9e6cc1ac42c68799865ec41ea58d89964c662a1273') # Enter your API token here
+provider = IBMQ.get_provider(hub='ibm-q')
+
+backend = provider.get_backend('ibmq_qasm_simulator') # Specifies the quantum device
+
+N = 179 * 239   # 42781
+a = 10
+
+factors = Shor(QuantumInstance(backend, shots=1, skip_qobj_validation=False))
+
+start_time = time.time()
+result_dict = factors.factor(N=N, a=a) # Where N is the integer to be factored
+execution_time = time.time() - start_time
+result = result_dict.factors
+
+print(result)
+print("Time:", execution_time)
